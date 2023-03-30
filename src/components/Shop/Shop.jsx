21aaaf22f0f79b835fff871109515f3ead37d729
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-03-30 18:35:39 +0600

    stored cart added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -7,20 +8,41 @@ const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
     }, [])
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+
+        // step 1: get id of the addedProduct
+        for (const id in storedCart) {
+            // step 2: get the product by using id
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                // step 3: get/add quantity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the added Product to the saved cart
+                savedCart.push(addedProduct);
+            }
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    }, [products])
 
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
     return (
         <div className='shop-container'>
-            <div className="products-container"> 
+            <div className="products-container">
                 {
                     products.map(product => <Product
                         key={product.id}
